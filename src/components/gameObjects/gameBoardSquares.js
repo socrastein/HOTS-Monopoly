9849d19be3794c57c73b58579d8ae0d703975bdd
socrastein <31764357+socrastein@users.noreply.github.com>
2023-05-11 00:16:07 -0600

    Add objects for property squares, fill in values

---
+++ src/components/gameObjects/gameBoardSquares.js
@@ -0,0 +1,483 @@
+const allSquares = [
+
+]
+
+const squareMediterraneanAve = {
+
+}
+
+const square1 = {
+  //Mediterranean Ave
+  boardIndex: 1,
+  name: " ",
+  group: 0,
+  price: 60,
+
+  rent: 2,
+  house: [
+    0, 
+    10,  //1 House
+    30,  //2 Houses
+    90,  //3 Houses
+    160, //4 houses
+    250, //5 houses (Hotel)
+  ],
+
+  housePrice: 50,
+  mortgage: 30,
+}
+
+const square2 = {
+  //Baltic Ave
+  boardIndex: 3,
+  name: " ",
+  group: 0,
+  price: 60,
+
+  rent: 4,
+  houses: [
+    0, 
+    20,  //1 House
+    60,  //2 Houses
+    180, //3 Houses
+    320, //4 houses
+    450, //5 houses (Hotel)
+  ],
+
+  housePrice: 50,
+  mortgage: 30,
+}
+
+////////////////////////////////
+
+const square3 = {
+  //Oriental Avenue
+  boardIndex: 2,
+  name: " ",
+  group: 1,
+  price: 100,
+
+  rent: 6,
+  houses: [
+    0, 
+    30,  //1 House
+    90,  //2 Houses
+    270, //3 Houses
+    400, //4 houses
+    550, //5 houses (Hotel)
+  ],
+
+  housePrice: 50,
+  mortgage: 50,
+}
+
+const square4 = {
+  //Vermont Ave
+  boardIndex: 0,
+  name: " ",
+  group: 1,
+  price: 100,
+
+  rent: 6,
+  houses: [
+    0, 
+    30,  //1 House
+    90,  //2 Houses
+    270, //3 Houses
+    400, //4 houses
+    550, //5 houses (Hotel)
+  ],
+
+  housePrice: 50,
+  mortgage: 50,
+}
+
+const square5 = {
+  //Connecticut Ave
+  boardIndex: 0,
+  name: " ",
+  group: 1,
+  price: 120,
+
+  rent: 8,
+  houses: [
+    0, 
+    40,  //1 House
+    100, //2 Houses
+    300, //3 Houses
+    450, //4 houses
+    600, //5 houses (Hotel)
+  ],
+
+  housePrice: 50,
+  mortgage: 60,
+}
+
+////////////////////////////////
+
+const square6 = {
+  //St Charles Place
+  boardIndex: 0,
+  name: " ",
+  group: 2,
+  price: 140,
+
+  rent: 10,
+  houses: [
+    0, 
+    50,  //1 House
+    150, //2 Houses
+    450, //3 Houses
+    625, //4 houses
+    750  //5 houses (Hotel)
+  ],
+
+  housePrice: 100,
+  mortgage: 70,
+}
+
+const square7 = {
+  //States Ave
+  boardIndex: 0,
+  name: " ",
+  group: 2,
+  price: 140,
+
+  rent: 10,
+  houses: [
+    0, 
+    50, //1 House
+    150, //2 Houses
+    450, //3 Houses
+    625, //4 houses
+    750, //5 houses (Hotel)
+  ],
+
+  housePrice: 100,
+  mortgage: 70,
+}
+
+const square8 = {
+  //Virginia Ave
+  boardIndex: 0,
+  name: " ",
+  group: 2,
+  price: 160,
+
+  rent: 12,
+  houses: [
+    0, 
+    60, //1 House
+    180, //2 Houses
+    500, //3 Houses
+    700, //4 houses
+    900, //5 houses (Hotel)
+  ],
+
+  housePrice: 100,
+  mortgage: 80,
+}
+
+////////////////////////////////
+
+const square9 = {
+  //St James Place
+  boardIndex: 0,
+  name: " ",
+  group: 3,
+  price: 180,
+
+  rent: 14,
+  houses: [
+    0, 
+    70, //1 House
+    200, //2 Houses
+    550, //3 Houses
+    750, //4 houses
+    950, //5 houses (Hotel)
+  ],
+
+  housePrice: 100,
+  mortgage: 90,
+}
+
+const square10 = {
+  //Tennessee Ave
+  boardIndex: 0,
+  name: " ",
+  group: 3,
+  price: 180,
+
+  rent: 14,
+  houses: [
+    0, 
+    70, //1 House
+    200, //2 Houses
+    550, //3 Houses
+    750, //4 houses
+    950, //5 houses (Hotel)
+  ],
+
+  housePrice: 100,
+  mortgage: 90,
+}
+
+const square11 = {
+  //New York Ave
+  boardIndex: 0,
+  name: " ",
+  group: 3,
+  price: 200,
+
+  rent: 16,
+  houses: [
+    0, 
+    80, //1 House
+    220, //2 Houses
+    600, //3 Houses
+    800, //4 houses
+    1000, //5 houses (Hotel)
+  ],
+
+  housePrice: 100,
+  mortgage: 100,
+}
+
+////////////////////////////////
+
+const square12 = {
+  //Kentucky Ave
+  boardIndex: 0,
+  name: " ",
+  group: 4,
+  price: 220,
+
+  rent: 18,
+  houses: [
+    0, 
+    90, //1 House
+    250, //2 Houses
+    700, //3 Houses
+    875, //4 houses
+    1050, //5 houses (Hotel)
+  ],
+
+  housePrice: 150,
+  mortgage: 110,
+}
+
+const square13 = {
+  //Indiana Ave
+  boardIndex: 0,
+  name: " ",
+  group: 4,
+  price: 220,
+
+  rent: 18,
+  houses: [
+    0, 
+    90, //1 House
+    250, //2 Houses
+    700, //3 Houses
+    875, //4 houses
+    1050, //5 houses (Hotel)
+  ],
+
+  housePrice: 150,
+  mortgage: 110,
+}
+
+const square14 = {
+  //Illinois Ave
+  boardIndex: 0,
+  name: " ",
+  group: 4,
+  price: 240,
+
+  rent: 20,
+  houses: [
+    0, 
+    100, //1 House
+    300, //2 Houses
+    750, //3 Houses
+    925, //4 houses
+    1100, //5 houses (Hotel)
+  ],
+
+  housePrice: 150,
+  mortgage: 120,
+}
+
+////////////////////////////////
+
+const square15 = {
+  //Atlantic Ave
+  boardIndex: 0,
+  name: " ",
+  group: 5,
+  price: 260,
+
+  rent: 22,
+  houses: [
+    0, 
+    110, //1 House
+    330, //2 Houses
+    800, //3 Houses
+    975, //4 houses
+    1150, //5 houses (Hotel)
+  ],
+
+  housePrice: 150,
+  mortgage: 130,
+}
+
+const square16 = {
+  //Ventnor Ave
+  boardIndex: 0,
+  name: " ",
+  group: 5,
+  price: 260,
+
+  rent: 22,
+  houses: [
+    0, 
+    110, //1 House
+    330, //2 Houses
+    800, //3 Houses
+    975, //4 houses
+    1150, //5 houses (Hotel)
+  ],
+
+  housePrice: 150,
+  mortgage: 130,
+}
+
+const square17 = {
+  //Marvin Gardens
+  boardIndex: 0,
+  name: " ",
+  group: 5,
+  price: 280,
+
+  rent: 24,
+  houses: [
+    0, 
+    120, //1 House
+    360, //2 Houses
+    850, //3 Houses
+    1025, //4 houses
+    1200, //5 houses (Hotel)
+  ],
+
+  housePrice: 150,
+  mortgage: 140,
+}
+
+////////////////////////////////
+
+const square18 = {
+  //Pacific Ave
+  boardIndex: 0,
+  name: " ",
+  group: 6,
+  price: 300,
+
+  rent: 26,
+  houses: [
+    0, 
+    130, //1 House
+    390, //2 Houses
+    900, //3 Houses
+    1100, //4 houses
+    1275, //5 houses (Hotel)
+  ],
+
+  housePrice: 200,
+  mortgage: 150,
+}
+
+const square19 = {
+  //North Carolina Ave
+  boardIndex: 0,
+  name: " ",
+  group: 6,
+  price: 300,
+
+  rent: 26,
+  houses: [
+    0, 
+    130, //1 House
+    390, //2 Houses
+    900, //3 Houses
+    1100, //4 houses
+    1275, //5 houses (Hotel)
+  ],
+
+  housePrice: 200,
+  mortgage: 150,
+}
+
+const square20 = {
+  //Pennsylvania Ave
+  boardIndex: 0,
+  name: " ",
+  group: 6,
+  price: 320,
+
+  rent: 28,
+  houses: [
+    0, 
+    150, //1 House
+    450, //2 Houses
+    1000, //3 Houses
+    1200, //4 houses
+    1400, //5 houses (Hotel)
+  ],
+
+  housePrice: 200,
+  mortgage: 160,
+}
+
+////////////////////////////////
+
+const square21 = {
+  //Park Place
+  boardIndex: 0,
+  name: " ",
+  group: 7,
+  price: 350,
+
+  rent: 35,
+  houses: [
+    0, 
+    175, //1 House
+    500, //2 Houses
+    1100, //3 Houses
+    1300, //4 houses
+    1500, //5 houses (Hotel)
+  ],
+
+  housePrice: 200,
+  mortgage: 175,
+}
+
+const square22 = {
+  //Boardwalk
+  boardIndex: 0,
+  name: " ",
+  group: 7,
+  price: 400,
+
+  rent: 50,
+  houses: [
+    0, 
+    200, //1 House
+    600, //2 Houses
+    1400, //3 Houses
+    1700, //4 houses
+    2000, //5 houses (Hotel)
+  ],
+
+  housePrice: 200,
+  mortgage: 200,
+}
