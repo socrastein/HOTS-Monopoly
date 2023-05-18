06851135b9595c00d1f652439a04c74d7a0190bb
socrastein <31764357+socrastein@users.noreply.github.com>
2023-05-17 20:27:27 -0600

    Add game menu options

--- src/components/gameState/gameState.js
+++ src/components/gameState/gameState.js
@@ -1,3 +1,10 @@
-const gameState = () => {
-  
+const gameState = {
+  numberOfPlayers: 2,
+  numberOfAI: 0,
+  isSpeedGame: false,
+  isFreeParkingBonus: false,
+  isHousingLimited: true,
+  isUnsoldPropertyAuctioned: false,
 }
+
+export default gameState;
