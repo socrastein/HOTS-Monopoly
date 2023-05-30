1634f6787a8324b0f394f78ae295db6960ea6aa8
socrastein <31764357+socrastein@users.noreply.github.com>
2023-05-29 18:36:18 -0600

    Begin adding commands to main start function

--- src/components/gameState/gameStart.js
+++ src/components/gameState/gameStart.js
@@ -1,7 +1,11 @@
 import gameState from "./gameState";
+import displayGameBoard from "../gameDisplay/displayGameBoard";
 
 const startGame = () => {
-
+  console.log("Starting new game");
+  const mainContainer = document.getElementById("mainContainer");
+  mainContainer.innerHTML = "";
+  displayGameBoard();
 }
 
 export default startGame;
