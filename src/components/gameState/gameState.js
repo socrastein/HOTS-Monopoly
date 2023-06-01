accaed9074a4e171200a472380b4c7076e177133
socrastein <31764357+socrastein@users.noreply.github.com>
2023-05-31 18:00:41 -0600

    Add properties for tracking player objects

--- src/components/gameState/gameState.js
+++ src/components/gameState/gameState.js
@@ -5,6 +5,13 @@ const gameState = {
   isFreeParkingBonus: false,
   isHousingLimited: true,
   isUnsoldPropertyAuctioned: false,
+
+  player1: undefined,
+  player2: undefined,
+  player3: undefined,
+  player4: undefined,
+  player5: undefined,
+  player6: undefined
 }
 
 export default gameState;
