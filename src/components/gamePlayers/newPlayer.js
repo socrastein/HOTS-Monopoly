f578bfaa0ae3831517181debd25f2ace35693998
socrastein <31764357+socrastein@users.noreply.github.com>
2023-05-31 18:02:56 -0600

    Refactor player objects to use icons & iconimages

--- src/components/gamePlayers/newPlayer.js
+++ src/components/gamePlayers/newPlayer.js
@@ -1,41 +1,51 @@
-const rollDice = (speed = no) => {
+import gameState from "../gameState/gameState";
+
+const diceRoll = (speed = false) => {
   let numDice;
-  speed ? numDice = 3 : numDice = 2;
+
+  if(speed){
+    (numDice = 3)
+  } else {
+    (numDice = 2);
+  } 
 
   let results = [];
-  for(i=0; i < numDice.length; i++){
+  for (let i = 0; i < numDice.length; i++) {
     let result = Math.floor(Math.random() * 6) + 1;
     results.push(result);
   }
   return results;
-}
+};
 
-const newPlayer = (name, avatar, token) => {
-  let player;
+const newPlayer = (name, iconName, iconImage) => {
+  let player = {};
 
-  player.rollDice = rollDice;
+  player.rollDice = () => diceRoll(gameState.isSpeedGame);
+
   player.location = 0;
   player.move = (spaces) => {
     player.location =+ spaces;
 
-    if(player.location > 39){
+    if (player.location > 39) {
       player.location = player.location - 40;
     }
 
-    if(player.location < 0){
+    if (player.location < 0) {
       player.location = player.location + 40;
     }
   };
-  
-  player.name = name;
-  player.avatar = avatar;
-  player.token = token;
 
+  player.name = name;
+  player.iconName = iconName;
+  player.iconImage = iconImage;
+  
   player.money = 1500;
   player.properties = [];
-  
+
   player.isBankrupt = false;
   player.isInJail = false;
 
   return player;
-}
+};
+
+export default newPlayer;
