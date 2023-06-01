ec16ef370c5ea56a165edfe850499e49ecc1fa25
socrastein <31764357+socrastein@users.noreply.github.com>
2023-05-31 18:02:21 -0600

    Build and style player HUD display

--- src/components/gameDisplay/displayGameHUD.js
+++ src/components/gameDisplay/displayGameHUD.js
@@ -1,15 +1,79 @@
-const displayBlankHeader = () => {
+import "./gameHud.css";
 
-}
+import gameState from "../gameState/gameState";
+import { propertyColors } from "./displaySpaceDetails";
 
-const displayPlayerNames = () => {
+const createPlayerContainers = () => {
+  for (let i = 1; i <= gameState.numberOfPlayers; i++) {
+    let container = document.createElement("div");
+    container.classList.add("playerHUDContainer");
+    container.id = `HUDplayer${i}`;
 
-}
+    let playerName = document.createElement("div");
+    playerName.textContent = `Player ${i}`;
+    playerName.classList.add("HUDName");
+    playerName.id = `HUDplayer${i}Name`;
 
-const displayPlayerFunds = () => {
+    let iconFundsContainer = document.createElement("div");
+    iconFundsContainer.classList.add("selectIconsContainer");
 
-}
+    let playerIcon = document.createElement("div");
+    playerIcon.classList.add("HUDIcon");
+    playerIcon.id = `HUDplayer${i}Icon`;
 
-const displayPlayerProperty = () => {
-  
-}
+    let playerFunds = document.createElement("div");
+    playerFunds.textContent = `1500`;
+    playerFunds.classList.add("HUDFunds");
+    playerFunds.id = `HUDplayer${i}Funds`;
+
+    iconFundsContainer.appendChild(playerIcon);
+    iconFundsContainer.appendChild(playerFunds);
+
+    let playerProperty = document.createElement("div");
+    playerProperty.classList.add("HUDProperties");
+    playerProperty.id = `HUDplayer${i}Property`;
+
+    container.appendChild(playerName);
+    container.appendChild(iconFundsContainer);
+    container.appendChild(playerProperty);
+
+    document.body.appendChild(container);
+  }
+};
+
+const displayPlayerProperties = () => {
+  for (let i = 1; i <= gameState.numberOfPlayers; i++) {
+    let player = gameState[`player${i}`];
+    if (!player) return;
+
+    let nameElement = document.getElementById(`HUDplayer${i}Name`);
+    let iconElement = document.getElementById(`HUDplayer${i}Icon`);
+    let fundsElement = document.getElementById(`HUDplayer${i}Funds`);
+    let propertyElement = document.getElementById(`HUDplayer${i}Property`);
+
+    nameElement.textContent = player.name;
+    iconElement.style.backgroundImage = `url(${player.iconImage})`;
+    fundsElement.textContent = player.money;
+    propertyElement.innerHTML = "";
+  }
+};
+
+const adjustPlayerFunds = (playerNumber, adjustment) => {
+  for (let i = 1; i <= gameState.numberOfPlayers; i++) {}
+};
+
+const displayPlayerProperty = (playerNumber) => {
+  return;
+};
+
+let HUD = false;
+
+const updateHUD = () => {
+  if (!HUD) {
+    createPlayerContainers();
+    HUD = true;
+  }
+  displayPlayerProperties();
+};
+
+export default updateHUD;
