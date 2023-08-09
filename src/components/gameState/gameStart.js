c862ba70bfb6312c8bc32c2be1eaaafc792c6d3f
socrastein <31764357+socrastein@users.noreply.github.com>
2023-08-08 20:00:55 -0600

    Add startFast option for quick launch w presets

--- src/components/gameState/gameStart.js
+++ src/components/gameState/gameStart.js
@@ -1,15 +1,38 @@
 import gameState from "./gameState";
-import updateHUD from "../gameDisplay/displayGameHUD";
-import playerSelectMenu from "../gameDisplay/menuPlayerSelect";
 import menuStart from "../gameDisplay/menuStart";
+
+import newPlayer from "../gamePlayers/newPlayer";
+import displayGameBoard from "../gameDisplay/displayGameBoard";
+import updateHUD from "../gameDisplay/displayGameHUD";
+import tank from "../gameDisplay/icons/tank.png";
+import support from "../gameDisplay/icons/support.png";
+
 
 const startGame = () => {
   console.log("Starting new game");
   const mainContainer = document.getElementById("mainContainer");
   mainContainer.innerHTML = "";
 
-  // menuStart();
-  playerSelectMenu(1);
+  // menuStart(); 
+  // Start a game from scratch with the Main Menu
+
+  startFast(); 
+  // Start a game with Matt & Summer presets
+
+}
+
+const startFast = () => {
+  console.log("Starting game with preset options")
+  const mainContainer = document.getElementById("mainContainer");
+  mainContainer.innerHTML = "";
+
+  let player1 = newPlayer("Matt", "Tank", tank);
+  let player2 = newPlayer("Summer", "Support", support)
+
+  gameState[`player1`] = player1;
+  gameState[`player2`] = player2;
+
+  displayGameBoard();
   updateHUD();
 }
 
